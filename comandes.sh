80aa43e320bda9f06d5da4ea98c3c760dcad760c
pilocoltin-a11y <pilocoltin@gmail.com>
2026-03-23 13:41:34 +0100

    dd

---
+++ comandes.sh
@@ -0,0 +1,16 @@
+mkdir documents
+cd documents
+mkdir Complexos
+mkdir Inequacions
+cd Complexos
+cp exercici 2.tex talleravaluable2
+cd ../
+cd Inequacions
+cp exercici1.tex talleravaluable2
+cd ../../
+mkdir imatges
+cd imatges
+touch info_imatge.tex
+echo "La imatge és de..." >> info_imatge.tex
+wget https://texample.net/files/rotated-triangle.png
+
